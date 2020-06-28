1585f2d06383ef757bd4a9e6de439070a47bafb4
Robert Brunhage <robertbrunhage@gmail.com>
2020-06-28 16:38:50 +0200

    Add more music that is randomized

--- lib/features/task_view/audio_service.dart
+++ lib/features/task_view/audio_service.dart
@@ -7,10 +7,19 @@ abstract class AudioService {
 
 class AssetAudioService implements AudioService {
   final assetsAudioPlayer = AssetsAudioPlayer();
+  final songs = <Audio>[
+    Audio("assets/music/in_the_moment_steve_oxen_1.mp3"),
+    Audio("assets/music/in_the_moment_steve_oxen_2.mp3"),
+    Audio("assets/music/in_the_moment_steve_oxen_3.mp3"),
+    Audio("assets/music/serenity_david_renda.mp3"),
+    Audio("assets/music/we_were_friends_david_fesliyan.mp3"),
+  ];
 
   Future<void> playMusic() async {
+    songs.shuffle();
+
     assetsAudioPlayer.open(
-      Audio("assets/music/test_music.mp3"),
+      Playlist(audios: songs),
       loopMode: LoopMode.playlist,
     );
   }
