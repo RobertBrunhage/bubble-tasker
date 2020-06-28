3000c7963ec81866da848d5c7adac21abce43566
Robert Brunhage <robertbrunhage@gmail.com>
2020-06-28 17:18:33 +0200

    Fix TextFormField design and validation

--- lib/features/add_task/add_task.dart
+++ lib/features/add_task/add_task.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_circular_slider/flutter_circular_slider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tasktimerconcept/features/add_task/add_task_view_model_provider.dart';
+import 'package:tasktimerconcept/shared/widgets/custom_raised_button.dart';
 
 //todo: handle overflow problems on smaller screens
 class AddTask extends StatefulWidget {
@@ -73,21 +74,28 @@ class _AddTaskState extends State<AddTask> {
           child: Column(
             children: [
               Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Color(0xff3A3C5F),
-                      border: InputBorder.none,
-                      hintText: 'Name',
-                      hintStyle: TextStyle(
-                        color: Colors.grey.shade400,
+                padding: const EdgeInsets.all(12.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(12.0),
                       ),
                     ),
-                    onSaved: (name) => addTaskViewModel.name = name,
+                    filled: true,
+                    fillColor: Color(0xff3A3C5F),
+                    hintText: 'Name',
+                    hintStyle: TextStyle(
+                      color: Colors.grey.shade400,
+                    ),
                   ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Write a name for your task';
+                    }
+                    return null;
+                  },
+                  onSaved: (name) => addTaskViewModel.name = name,
                 ),
               ),
               SizedBox(height: 16),
@@ -106,8 +114,7 @@ class _AddTaskState extends State<AddTask> {
                     // a = ?, b = position, c = ?
                     onSelectionChange: (a, b, c) {
                       calculateDurationFromPosition(b);
-                      addTaskViewModel.duration =
-                          Duration(minutes: durationInMinutes);
+                      addTaskViewModel.duration = Duration(minutes: durationInMinutes);
                     },
                   ),
                 ],
@@ -118,20 +125,14 @@ class _AddTaskState extends State<AddTask> {
                 child: Row(
                   children: [
                     Expanded(
-                      child: Container(
-                        height: 50,
-                        child: RaisedButton(
-                          color: Color(0xff3A3C5F),
-                          textColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Text('Create'),
-                          onPressed: () {
-                            addTaskViewModelProvider.read(context).saveForm();
-                            Navigator.pop(context);
-                          },
-                        ),
+                      child: CustomRaisedButton(
+                        text: "Create",
+                        onTap: () async {
+                          final shouldPop = await addTaskViewModelProvider.read(context).saveForm();
+                          if (shouldPop) {
+                            Navigator.of(context).pop();
+                          }
+                        },
                       ),
                     ),
                   ],
